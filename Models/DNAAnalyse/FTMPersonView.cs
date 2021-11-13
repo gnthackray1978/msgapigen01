880e8d1ca11bc96b4f3b33022e2de8a1b5b82b5b
George <gnthackray@btinternet.com>
2021-11-13 23:09:03 +0000

    Update FTMPersonView.cs
    getting rid of nulls

--- Models/DNAAnalyse/FTMPersonView.cs
+++ Models/DNAAnalyse/FTMPersonView.cs
@@ -2,7 +2,7 @@
 
 namespace AzureContext.Models
 {
-    public partial class FTMPersonView : IYearRange, IName, Ilocation, IOrigin
+    public partial class FTMPersonView : IYearRange, IName, IPreciseLocation, IOrigin
     {
         public int Id { get; set; }
 
@@ -13,13 +13,13 @@ namespace AzureContext.Models
         public int YearTo { get; set; }
 
         public string Location { get; set; }
-        public double? BirthLat { get; set; }
-        public double? BirthLong { get; set; }
+        public double BirthLat { get; set; }
+        public double BirthLong { get; set; }
 
         public string AltLocationDesc { get; set; }
         public string AltLocation { get; set; }
-        public double? AltLat { get; set; }
-        public double? AltLong { get; set; }
+        public double AltLat { get; set; }
+        public double AltLong { get; set; }
 
         public string Origin { get; set; }
         public int PersonId { get; set; }
