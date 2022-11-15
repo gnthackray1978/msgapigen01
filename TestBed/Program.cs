fbd6c4a15412a00dc0bae436614e53f26ba62546
George <gnthackray@btinternet.com>
2022-11-15 14:03:47 +0000

    test stuff

--- TestBed/Program.cs
+++ TestBed/Program.cs
@@ -3,26 +3,73 @@ using ConfigHelper;
 using System;
 using System.Linq;
 using Api.DB;
+using Api.Services;
+using Api.Types.RequestQueries;
 using static Api.Services.DiagramService;
 
 namespace TestBed
 {
+    public class config : IMSGConfigHelper
+    {
+        public string ClientURLs { get; set; }
+        public string AuthServerUrl { get; set; }
+        public string MSGApiGenUrl { get; set; }
+        public string MSGGenDB01 { get; set; } = Secrets.ConnectionString;
+    }
+
     class Program
     {
   
         static void Main(string[] args)
         {
             var az = new AzureDBContext(Secrets.ConnectionString);
-           // var msgConfigHelper = new MSGConfigHelper();
+            // var msgConfigHelper = new MSGConfigHelper();
+
+
+            TestFTMLatLngList(az);
+
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+
+        }
+
+        private static void TestGenerateDescendantGraph(AzureDBContext az)
+        {
             var b = new DescendantGraphBuilder(az);
 
             var tree = az.TreeRecord.FirstOrDefault(f => f.Name == "_34_Kennington");
 
-            if(tree!=null)
+            if (tree != null)
                 b.GenerateDescendantGraph(3217, tree.ID);
-            
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        }
+
+        private async static void TestFTMLatLngList(AzureDBContext az)
+        {
+            var c = new config() { };
+
+
+            var b = new DNAAnalyseService(null,null,c);
+
+            // var tree = az.TreeRecord.FirstOrDefault(f => f.Name == "_34_Kennington");
+
+           
+            var treeDictionary = az.TreeRecord.ToDictionary(p => p.ID, p => p.Name);
+
+            treeDictionary.Add(0, "Unknown");
+
+            var p = new DNASearchParamObj
+            {
+                Groupings = az.TreeRecordMapGroup.ToList(),
+                YearFrom = 1500,
+                YearTo = 1900,
+                Origin = "300"
+            };
+
+
+            await b.FTMLatLngList(p);
 
 
         }
